b2bfdeb8f650dfb0e754d07604cf5bbbb6c8e2a3
Marcus M. Darden <marcus.darden@cs.olivetcollege.edu>
2013-09-27 19:57:30 -0400

    Update Twitter app to use current Twitter API.

--- example/tweet.py
+++ example/tweet.py
@@ -31,7 +31,7 @@ oauth = OAuth()
 # user interface on the twitter side.
 
 twitter = oauth.remote_app('twitter',
-                           base_url='https://api.twitter.com/1/',
+                           base_url='https://api.twitter.com/1.1/',
                            request_token_url='https://api.twitter.com/oauth/request_token',
                            access_token_url='https://api.twitter.com/oauth/access_token',
                            authorize_url='https://api.twitter.com/oauth/authenticate',
