4b94a446334ef12a8231b6034902db6f9e5e7722
Marcus M. Darden <marcus.darden@cs.olivetcollege.edu>
2013-10-01 13:41:14 -0400

    Clean up version that runs both facebook and google OAuth.
    Login is accomplished at http://site/login/facebook
                          or http://site/login/google
    
    Sessions can be reset at http://site/reset

--- example/dual.py
+++ example/dual.py
@@ -2,7 +2,6 @@ from flask import Flask, redirect, url_for, session, request
 from flask_oauth import OAuth
 
 
-
 # You must configure these 3 values from Google APIs console
 # https://code.google.com/apis/console
 GOOGLE_CLIENT_ID = '87105594236.apps.googleusercontent.com'
@@ -19,6 +18,7 @@ app.debug = DEBUG
 app.secret_key = SECRET_KEY
 oauth = OAuth()
 
+method = ''
 facebook = oauth.remote_app('facebook',
                             base_url='https://graph.facebook.com/',
                             request_token_url=None,
@@ -41,65 +41,62 @@ google = oauth.remote_app('google',
                           consumer_secret=GOOGLE_CLIENT_SECRET)
 
 
-@app.route('/facebook')
-def facebook_index():
-    access_token = session.get('access_token')
-    if access_token is None:
-        return redirect(url_for('facebook_login'))
+@app.route('/userinfo')
+@app.route('/userinfo/<access_token>')
+def userinfo(access_token=None):
+    print 'userinfo', access_token, method
+    if method == 'google':
+        # Site specific stuff for google user info
+        from urllib2 import Request, urlopen, URLError
 
-    me = facebook.get('/me')
-    return str(me.data)  # Show all data in python dictionary
+        headers = {'Authorization': 'OAuth '+access_token}
+        req = Request('https://www.googleapis.com/oauth2/v2/userinfo',
+                      None, headers)
+        try:
+            res = urlopen(req)
+        except URLError, e:
+            if e.code == 401:
+                # Unauthorized - bad token
+                session.pop(method+'_access_token', None)
+                return redirect(url_for('login'))
+            return res.read()
 
-
-@app.route('/google')
-def google_index():
-    access_token = session.get('access_token')
-    if access_token is None:
-        return redirect(url_for('google_login'))
-
-    access_token = access_token[0]
-    from urllib2 import Request, urlopen, URLError
-
-    headers = {'Authorization': 'OAuth '+access_token}
-    req = Request('https://www.googleapis.com/oauth2/v2/userinfo',
-                  None, headers)
-    try:
-        res = urlopen(req)
-    except URLError, e:
-        if e.code == 401:
-            # Unauthorized - bad token
-            session.pop('access_token', None)
-            return redirect(url_for('google_login'))
         return res.read()
-
-    return res.read()
-
-
-@app.route('/facebook-login')
-def facebook_login():
-    callback=url_for('facebook_authorized',
-                     next=request.args.get('next') or request.referrer or None,
-                     _external=True)
-    return facebook.authorize(callback=callback)
+    elif method == 'facebook':
+        # Site specific stuff for facebook user info
+        me = facebook.get('/me')
+        return str(me.data)  # Show all data in python dictionary
+    else:
+        return redirect(url_for('invalid'))
 
 
-@app.route('/google-login')
-def google_login():
-    callback = url_for('authorized', _external=True)
-    return google.authorize(callback=callback)
+@app.route('/login/<auth_method>')
+def index(auth_method):
+    global method
+    method = auth_method
+    print 'METHOD='+method
+    access_token = session.get(method+'_access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+
+    return redirect(url_for('userinfo', access_token=access_token[0]))
 
 
-@app.route(REDIRECT_URI)
-@google.authorized_handler
+@app.route('/login')
+def login():
+    if method == '' or method is None:
+        return redirect(url_for('invalid'))
+
+    callback = url_for(method+'_authorized', _external=True)
+
+    if method == 'facebook':
+        return facebook.authorize(callback=callback)
+    elif method == 'google':
+        return google.authorize(callback=callback)
+
+
 def authorized(resp):
-    access_token = resp['access_token']
-    session['access_token'] = access_token, ''
-    return redirect(url_for('google_index'))
-
-
-@app.route('/login/authorized')
-@facebook.authorized_handler
-def facebook_authorized(resp):
+    print 'AUTHORIZED', resp
     if resp is None:
         return 'Access denied: reason=%s error=%s' % (
             request.args['error_reason'],
@@ -107,14 +104,47 @@ def facebook_authorized(resp):
         )
 
     access_token = resp['access_token']
-    session['access_token'] = access_token, ''
-    return redirect(url_for('facebook_index'))
+    session[method+'_access_token'] = access_token, ''
+    return redirect(url_for('userinfo', access_token=access_token))
+
+
+@app.route('/login/google/authorized')
+@google.authorized_handler
+def google_authorized(resp):
+    return authorized(resp)
+
+
+@app.route('/login/facebook/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    return authorized(resp)
+
+
+def get_access_token():
+    return session.get(method+'_access_token')
 
 
 @facebook.tokengetter
+def facebook_tokengetter():
+    return get_access_token()
+
+
 @google.tokengetter
-def get_access_token():
-    return session.get('access_token')
+def google_tokengetter():
+    return get_access_token()
+
+
+# Some extra urls to control the app and respond
+@app.route('/invalid')
+def invalid():
+    return 'Invalid'
+
+
+@app.route('/reset')
+def reset():
+    session['google_access_token'] = None
+    session['facebook_access_token'] = None
+    return 'Sessions reset'
 
 
 if __name__ == '__main__':
